d6389699180d0e58866c2e2f69bf0f8c22a11d6a
Andres A <ing.andres.avenda@gmail.com>
2025-01-15 07:46:30 -0500

    feat: Configuracion Security, y login, creacion entidad user

--- backend/NoCountry-Backend/src/main/java/nocountry/parquedediversiones/demo/DemoApplication.java
+++ backend/NoCountry-Backend/src/main/java/nocountry/parquedediversiones/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package nocountry.parquedediversiones.demo;
 
+import jakarta.transaction.Transactional;
+import org.apache.catalina.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
---
+++ backend/NoCountry-Backend/src/main/java/nocountry/parquedediversiones/demo/entities/Usuario.java
@@ -0,0 +1,31 @@
+package nocountry.parquedediversiones.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import nocountry.parquedediversiones.demo.enums.RoleName;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol", nullable = false)
+    private RoleName rol;
+
+    @Column(name = "nombre", nullable = false, unique = true)
+    private String nombre;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "contraseña", nullable = false)
+    private String contraseña;
+
+//    @OneToOne(mappedBy = "Usuario")
+//    private Venta venta;
+}
